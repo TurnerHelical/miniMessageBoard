ac97c68c500743df2ec6104303fc50c2579b0edb
TurnerHelical <hunterde91@gmail.com>
2025-10-23 14:46:14 -0400

    added notes to homeRoutes for future use

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,8 +1,32 @@
+// this line is again making the Router object available to this file, so it can handle routing
 const {Router} = require('express');
+
+// this line create a new Router instance that will handle the routing for specifically the routes listed in this file, which allows you to seperate out the routes into 
+// modules that are grouped by the request URL starting point
 const router = Router();
 
+// !!IMPORTANT!! this line defines a route, the .get handles any get requests that are sent to this route, you use router.get as .get is a method on the router object
+// other methods are .post, .put, .delete.
+// the '/' is the path that this route handles, for instance localhost:3000/ would follow this route, for this router you won't see many other routes as it is linked to
+// the homepage
+// the (req, res) => {} is the route handler function and defines what happens when this route is hit by a request.
+// the req is the Request object and holds the information about the incoming request, including the URL, query params, headers, form data, and others
+// the res is the response object that controls how you send data back to the client, you can use several methods with res in the function including , 
+// res.send() which will send plain text or can send HTML snippet,
+// res.json() will send back pure JSON, res.json is used to send an object back to the client, usually an API, or a Front-end handler
 router.get('/', (req, res) => {
+    
+    // res.render is for use with view engines, it informs express to look for the view engine config in your app.js so it knows which view engine to use and where to find 
+    // templates. This specific call looks for a views/index.ejs file, passes the { title: 'Home Page' } to that view which allows the view engine, EJS, to use those 
+    // variables to dynamically generate html, in this case the passed information would be used in the HTML head to change the title of the webpage to 'Home Page'.
+    // after the HTML is rendered with the passed variables, express send the complete HTML file to the browser
     res.render('index', { title: 'Home Page' });
 });
 
+// this line exports this router so it can be used by the main index router to handle requests
 module.exports = router;
+
+// this router will only handle request that match '/' with the current setup, if I wanted to I could define more routes in this file to handle say '/team' requests and 
+// if I also have a seperate router for the /team url it won't be reached as express will pass the request to the first middleware that matches the url and then stop 
+// any attempt to match any of the other routers. But if the home router has a '/team' route defined and the team router has a '/team/about' and the request is '/team/about'
+// express will look in the home foldere first see that /about matches but not /about/team so the request will move to the next middleware in line and continue to match paths
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,23 @@
-
+// this line allows the app to use the special Router object which allows you to break routes into smaller cleaner modules
 const { Router } = require('express');
+
+// these lines import the other route modules so that this page can send the request to the right file
+// the require should be the path to the route module
+// the individual route modules need to be exported to be accessed here
 const home = require('./homeRoutes');
 const message = require('./messageRoutes')
 
+// this creates the main router, it ties together all the other route modules 
 const router = Router();
 
+// these lines define the routes to the individual route modules
+// any request that starts with / will be passed along to the homeRouter which will then perform the routes associated with it
 router.use('/', home);
+
+// this line means that any request that starts with /message will be sent to the message router which was required at the top of this page
+// when the index router passes this along to the message router, the request will remove the /message part, so on the message router '/' would be equal to /message
+// and any other sub-routes would be '/someOtherThing' example would be '/new' if I wanted to create a new message route, the base request would be '/message/new'
 router.use('/message', message);
 
+// this line exports the router so app.js can access it and can pass the requests here for further routing
 module.exports = router;
